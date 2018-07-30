f19b2ff45a4bf5b8ae2ea5abf550c99c9aee579b
Taishawn Blackmon <taishawnblackmon@gmail.com>
2018-07-30 19:53:19 -0400

    adding java script lab 1

--- Javascript-Lab1.js
+++ Javascript-Lab1.js
@@ -3,16 +3,16 @@ const myName2 = prompt ("Please enter another name.") ;
 const myName3 = prompt ("Please enter one more name.") ;
 
 if (myName1.length === myName2.length & myName1.length === myName3.length) {
-  console.log (myName1 + " " + myName2 + " " + myName3 + " are the same length.")
+  console.log ("All three names, " + myName1 + " " + myName2 + " " + myName3 + " are the same length.")
 } 
 else if (myName1.length > myName2.length & myName1.length > myName3.length) {
   console.log (myName1 + " is the longer name")
 }
 else if (myName1.length === myName2.length) {
-  console.log (myName1 + " " + myName2 + " are the longer names.")
+  console.log (myName1 + " & " + myName2 + " are tied for the longest names.")
 }
 else if (myName1.length === myName3.length) {
-  console.log (myName1 + " " + myName3 + " are the longer names")
+  console.log (myName1 + " & " + myName3 + " are tied for the longest names")
 }
 else if (myName2.length > myName1.length & myName2.length > myName3.length) {
   console.log (myName2 + " is the longer name.")
@@ -21,5 +21,5 @@ else if (myName3.length > myName2.length & myName3.length > myName1.length) {
   console.log (myName3 + " is the longer name.")
 } 
 else if (myName3.length === myName2.length) {
-  console.log (myName3 + " " + myName2 + " are the longer names.")
+  console.log (myName3 + " & " + myName2 + " are tied the longest names.")
 }
